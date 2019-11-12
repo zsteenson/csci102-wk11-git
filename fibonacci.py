00b932e3194528fde89b383f294a4a653eae6f41
Zachary Steenson <zsteenson@mines.edu>
2019-11-12 10:08:49 -0700

    Created filethat calculates the first 10 fibonacci numbers

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,9 @@
 def fib():
-    fibs = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    fibs = [1, 2]
 
     for i in range(1,9):
-
-        ''' 
-        implement Fibonacci sequence to calculate the 
-        first 10 Fibonacci numbers, note Fn = Fn-1 + Fn-2
-        '''
-
+        fibs.append(fibs[i] + fibs[i - 1])
+    
     return fibs
 
 def main():
